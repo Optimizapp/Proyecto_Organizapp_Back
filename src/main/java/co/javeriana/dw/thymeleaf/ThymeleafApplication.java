ae5164576b79c9f4c696ae30ec896b1f18515a6a
Diego <za_diego@javeriana.edu.co>
2026-03-05 22:49:59 -0500

    Fix JPA package scanning for Company entity and repository

--- src/main/java/co/javeriana/dw/thymeleaf/ThymeleafApplication.java
+++ src/main/java/co/javeriana/dw/thymeleaf/ThymeleafApplication.java
@@ -2,8 +2,12 @@ package co.javeriana.dw.thymeleaf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "co.javeriana.dw")
+@EntityScan(basePackages = "co.javeriana.dw.organizapp.entity")
+@EnableJpaRepositories(basePackages = "co.javeriana.dw.organizapp.repository")
 public class ThymeleafApplication {
 
 	public static void main(String[] args) {
